9701a68373e1e82f2a7cf6de3dc0f81ae164b310
Alain Mazy <am@osimis.io>
2020-09-10 09:39:02 +0200

    update sync azure

--- Scripts/Migration/2020-09-08-TransferToAzure.sh
+++ Scripts/Migration/2020-09-08-TransferToAzure.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-##
-## Contribution by Steve Hawes
-##
-## It is meant to migrate orthanc files from a FileSystem storage to
-## an Azure Blob Storage such that it can be used by the Orthanc Azure Blob Storage plugin.
-## The script adapts the file structure and naming from hierarchical (FileSystem)
-## to flat (Blob storage)
-##
 
 usage() {
-  echo -e "Usage:\t$0 -s <source-path> -a <storage-account-name> -c <container-name> -r"
+  echo -e "Usage:\t$0 -s <source-path> -a <storage-account-name> -c <container-name> [-t <SASToken>] [-l <main|sub|off>]"
   echo -e "Where: "
   echo -e "  -s\tthe source directory to copy from, usually the root of the Orthanc storage area"
   echo -e "  -a\tthe Azure storage account name"
   echo -e "  -c\tthe Azure storage container name - this container MUST already exist"
-  echo -e "  -r\toptional paramater to indicate that the specified directory should be copied recursively"
+  echo -e "  -l\toptional parameter to specify if a local copy of the files should be made and then transferred as a batch. Using this option with main or sub specified will make local copies of the files with the correct file extension and then transfer them in a batch so you must have enough space in your /tmp partition to hold a copy of the largest subdirectory to be copied"
+  echo -e "\t<main>\t-\tbatch the files by grouping them according to the highest level directory within the source path (requires most free space)"
+  echo -e "\t<sub>\t-\tbatch the files by grouping them according to the lowest level directory within the source path (requires least free space)"
+  echo -e "\t<off>\t-\tdo not batch the files and transfer them individually (requires no free space, default value)"
+  echo -e "  -t\toptional parameter giving a SAS token for the Azure storage - this removes the need to login via browser"
   exit 1
 }
 
@@ -24,15 +20,33 @@ then
     exit
 fi
 
-RECURSIVE="-maxdepth 1"
+RECURSIVE=
+SASTOKEN=
+LOCALCOPY=false
 
-while getopts 's:a:c:r' opt
+while getopts 'l:s:a:c:t:' opt
 do
   case ${opt} in
     s) SOURCE=$OPTARG ;;
     a) ACCOUNTNAME=$OPTARG ;;
     c) CONTAINERNAME=$OPTARG ;;
     r) RECURSIVE='' ;;
+    t) SASTOKEN=$OPTARG ;;
+    l) 
+       case ${OPTARG} in
+         main) LOCALCOPY=true
+               RECURSIVE="-maxdepth 0"
+               ;;
+         sub) LOCALCOPY=true
+              RECURSIVE=""
+              ;;
+         off) LOCALCOPY=false
+              RECURSIVE=""
+              ;;
+         *) usage ;;
+       esac
+       ;;
+    *) usage ;;
   esac
 done
 
@@ -41,26 +55,60 @@ then
   usage
 fi
 
-azcopy login
+if [ -z "${SASTOKEN}" ]
+then
+  azcopy login
+fi
 
 if [ "$?" == 0 ] 
 then
-  for a in $(find ${SOURCE} ${RECURSIVE} -type f -print)
-  do
-    case $(file -b --mime-type $a) in
-      application/dicom) EXTENSION=.dcm ;;
-      text/plain) EXTENSION=.json ;;
-      *) EXTENSION="" ;;
-    esac
-    BLOBFILE=$(basename $a)${EXTENSION}
-    azcopy copy $a https://${ACCOUNTNAME}.blob.core.windows.net/${CONTAINERNAME}/${BLOBFILE}
-    if [ $? -gt 0 ]
-    then
-      exit 1
-    fi
-  done
+  if [ "${LOCALCOPY}" == true ]
+  then
+    for dir in  $(find ${SOURCE} ${RECURSIVE} -type d -print | sort)
+    do
+      echo "Starting local copy of ${dir}"
+      TMPDIR=$(mktemp -d -t az-XXXXXXXXXX)
+      for a in $(find ${dir} -type f -print | sort)
+      do
+        case $(file -b --mime-type $a) in
+          application/dicom) EXTENSION=.dcm ;;
+          text/plain) EXTENSION=.json ;;
+          *) EXTENSION="" ;;
+        esac
+        BLOBFILE=$(basename $a)${EXTENSION}
+        cp $a ${TMPDIR}/${BLOBFILE}
+        if [ $? -gt 0 ]
+        then
+          exit 1
+        fi
+      done
+      echo "Local copy complete"
+      azcopy copy "${TMPDIR}/*" https://${ACCOUNTNAME}.blob.core.windows.net/${CONTAINERNAME}/${SASTOKEN} --overwrite false --recursive
+      if [ $? -gt 0 ]
+      then
+        exit 1
+      fi
+      rm -rf ${TMPDIR}
+    done
+  else
+    for a in $(find ${SOURCE} ${RECURSIVE} -type f -print | sort)
+    do
+      case $(file -b --mime-type $a) in
+        application/dicom) EXTENSION=.dcm ;;
+        text/plain) EXTENSION=.json ;;
+        *) EXTENSION="" ;;
+      esac
+      BLOBFILE=$(basename $a)${EXTENSION}
+      echo azcopy copy $a https://${ACCOUNTNAME}.blob.core.windows.net/${CONTAINERNAME}/${BLOBFILE}${SASTOKEN} --overwrite false
+      if [ $? -gt 0 ]
+      then
+        exit 1
+      fi
+    done
+  fi
 fi
 
-
-
-azcopy logout
+if [ -z "${SASTOKEN}" ]
+then
+  azcopy logout
+fi
